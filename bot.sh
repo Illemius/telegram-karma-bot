0894b5ff37d49fe354daeedd469e978b1e23246a
Alex Root Junior <jroot.junior@gmail.com>
2016-10-31 17:07:39 +0200

    Init script

--- bot.sh
+++ bot.sh
@@ -7,34 +7,34 @@ MAIN="main.py"
 TRANSLATE_LIB_URL="git@bitbucket.org:illemius/translatelib.git"
 case $1 in
     setup)
-        git clone $TRANSLATE_LIB_URL TranslateLib
+        git clone ${TRANSLATE_LIB_URL} TranslateLib
         cp config.example.py config.py
         nano config.py
     ;;
     start)
-        $PYTHON $MAIN
+        ${PYTHON} ${MAIN}
     ;;
     background|bg)
         if  [ -f "$PID_FILE" ]; then
-            PID=`cat $PID_FILE`
-            if ps -p $PID > /dev/null; then
+            PID=`cat ${PID_FILE}`
+            if ps -p ${PID} > /dev/null; then
                 echo "Bot already started! " >&2
                 exit
             fi
         fi
         echo "Run in background"
-        $PYTHON $MAIN &
+        ${PYTHON} ${MAIN} &
         MyPID=$!
         echo "-> [$MyPID]"
-        echo $MyPID > $PID_FILE
+        echo ${MyPID} > ${PID_FILE}
         exit
     ;;
     stop)
         if ! [ -f "$PID_FILE" ]; then
             echo "PID file not found."
         else
-            PID=`cat $PID_FILE`
-            if ! kill $PID > /dev/null 9>&1; then
+            PID=`cat ${PID_FILE}`
+            if ! kill ${PID} > /dev/null 9>&1; then
                 echo "Could not send SIGTERM to process $PID" >&2
             else
                 echo "Killed."
